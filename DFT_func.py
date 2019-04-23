dc92f9989331df06383ef373a6e837643cc5f3ee
gsdevakumar <49908505+gsdevakumar@users.noreply.github.com>
2019-04-23 17:53:53 +0530

    Add files via upload

--- DFT_func.py
+++ DFT_func.py
@@ -9,11 +9,8 @@ Created on Fri Mar  1 14:09:44 2019
 import numpy as np
 from matplotlib import pyplot as plt
 import cmath as c
-j=c.sqrt (-1)
-t=np.linspace (0,2,50)
-x=np.sin (2*np.pi*t)
-#x=[1,2,3,4,5]
 def dft (x):
+    j=c.sqrt (-1)
     l= len (x)
     b=[]
     p=[]
@@ -25,12 +22,5 @@ def dft (x):
         b.append (s)
         p.append (abs(s))
         q.append (np.angle(s))
-dft(x)
-plt.subplot (411)
-plt.stem (t,x)
-plt.subplot (412)
-plt.stem (b)
-plt.subplot (413)
-plt.stem (p)
-plt.subplot (414)
-plt.stem (q)
+    return b
+
